460de43f56ff325a65f8cf7c266e04e7f364fe38
MeikeSteinhilber <Meike.Steinhilber@aol.com>
2026-01-15 11:53:00 +0100

    add README

--- R/test_sample_size_tool.R
+++
@@ -1,8 +0,0 @@
-
-# install.packages("devtools")
-# install.packages("remotes")
-remotes::install_github("MeikeSteinhilber/sprtt", ref = "develop")
-
-library(sprtt)
-
-sprtt::plan_sample_size(0.2, 3, power = .95, output_dir = ".")
